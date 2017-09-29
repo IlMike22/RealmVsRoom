b066e47d8c270f427ea8d4fd09c71fcaa7282d17
MWidlok <m.widlok@immowelt.de>
2017-09-29 16:16:58 +0200

    room async

--- AndroidDev/RealmSample/app/src/main/java/com/example/mwidlok/realmsample/RoomActivity.java
+++ AndroidDev/RealmSample/app/src/main/java/com/example/mwidlok/realmsample/RoomActivity.java
@@ -1,13 +1,17 @@
 package com.example.mwidlok.realmsample;
 
 import android.arch.persistence.room.Room;
+import android.os.AsyncTask;
 import android.support.constraint.solver.ArrayLinkedVariables;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Toast;
 
+import com.example.mwidlok.realmsample.dao.DaoAccess;
 import com.example.mwidlok.realmsample.database.MyDatabase;
 import com.example.mwidlok.realmsample.model.PersonRoom;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class RoomActivity extends AppCompatActivity {
@@ -20,15 +24,32 @@ public class RoomActivity extends AppCompatActivity {
         setContentView(R.layout.activity_room);
 
         // setup database.
-        myDatabase = Room.databaseBuilder(getApplicationContext(), MyDatabase.class, "myDb").allowMainThreadQueries().build();
+        // allowMainThreadQueries() möglich, aber nicht zu empfehlen.
+        //myDatabase = Room.databaseBuilder(getApplicationContext(), MyDatabase.class, "myDb").allowMainThreadQueries().build();
+        myDatabase = Room.databaseBuilder(getApplicationContext(), MyDatabase.class, "myDb").build();
 
+        new AsyncTask<Void,Void,Boolean>()
+        {
+            final PersonRoom person = new PersonRoom("Max","Mustermann",42);
+            @Override
+            protected Boolean doInBackground(Void... voids) {
+                myDatabase.daoAccess().insertPerson(person);
+                return true;
+            }
+
+            protected void onPostExecute()
+            {
+                // getting data from database.
+                List<PersonRoom> persons = myDatabase.daoAccess().getAllPersons();
+            }
+
+        }.execute();
         // setup entity.
-        PersonRoom person = new PersonRoom("Max","Mustermann",42);
+
 
         // write entity via dao in database.
-        myDatabase.daoAccess().insertPerson(person);
 
-        // getting data from database.
-        List<PersonRoom> persons = myDatabase.daoAccess().getAllPersons();
+
+
     }
 }
